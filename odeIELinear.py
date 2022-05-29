530e6d4d50ccebc32402fe464c181d97d8b3327a
Richard Amering <richard.amering@gmx.at>
2022-05-29 19:44:27 +0200

    added implicit euler solver for linear systems only

---
+++ odeIELinear.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 17:02:51 2022
+
+@author: Richard
+"""
+
+import numpy as np
+
+
+
+
+def odeIELinear(coeff_matrix, t, y0):
+    """Implicit Euler solver for linear systems of ODEs. 
+    Expects a coefficient matrix instead of a modelfunction, contrary to other ODE solvers.
+    """
+    
+    timevalues = np.array(t).flatten()
+    timedeltas = np.diff(timevalues)
+    
+    y0 = np.array(y0).reshape([-1, 1])
+    y = np.zeros([y0.size, timevalues.size])
+    yi = y0
+    I = np.identity(y0.size)
+    
+    for ti, dt in (iterator:=np.nditer([timevalues[..., :-1], timedeltas], flags=['c_index'])):
+        i = iterator.index
+        y[:, i, np.newaxis] = yi
+        yi = np.linalg.inv(I - coeff_matrix*dt) @ yi
+    
+    y[:, i+1, np.newaxis] = yi
+    return timevalues, y
